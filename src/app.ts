58e7e891b199080a2c34f82d57a3091c322ffa5a
Khanis Anthony <khanisanthony93@gmail.com>
2021-08-17 17:25:44 +0700

    Install eslint airbnb rules

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,17 @@
-"use strict"
-
 import express from 'express';
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-// const express = require("express");
-// const path = require("path");
-// const cookieParser = require("cookie-parser");
-// const logger = require("morgan");
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.set('views', path.join(__dirname, "../", "views"));
+app.set('views', path.join(__dirname, '../', 'views'));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response, NextFunction } from "express";
-var router = express.Router();
+import express, { Request, Response, NextFunction } from 'express';
+
+const router = express.Router();
 
 /* GET users listing. */
 // router.get('/', function (req: Request, res: Response, next: NextFunction) {
-router.get('/', function (req, res, next) {
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+export default router;
